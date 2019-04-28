26e923f6e06d797236f89b1f414bd544eded7965
Yoofi96 <joseph.brown-pobee@ashesi.edu.gh>
2019-04-28 19:52:58 +0000

    Contact Picker 2

--- www/js/index.js
+++ www/js/index.js
@@ -43,7 +43,18 @@ var failedCallback = function(result){
 }
 
 function ContactPicker(){
-    window.plugins.contactNumberPicker.pick(successCallback,failedCallback);
+  navigator.contactsPhoneNumbers.list(function(contacts) {
+      console.log(contacts.length + ' contacts found');
+      for(var i = 0; i < contacts.length; i++) {
+         console.log(contacts[i].id + " - " + contacts[i].displayName);
+         for(var j = 0; j < contacts[i].phoneNumbers.length; j++) {
+            var phone = contacts[i].phoneNumbers[j];
+            console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber+ ")");
+         }
+      }
+   }, function(error) {
+      console.error(error);
+   });
 }
 
 function handleOffline() {
